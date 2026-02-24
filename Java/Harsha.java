e1caafd568bf041e972d9ccc8a7e1b055194bb51
Harsha-Valluri-08 <harshvalluri08@gmail.com>
2026-02-24 20:21:22 +0530

    changes

--- Java/Harsha.java
+++ Java/Harsha.java
@@ -1,11 +1,9 @@
-import java.util.Scanner;
-
 public class Harsha {
     public static void main(String[] args){
-        System.out.print("Enter your name: ");
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println("Hello, "+s);
-        sc.close();
+        add(5,5);
     }
+
+    static void add(int a, int b){
+    System.out.println(a+b);
+}
 }
